476566925bd9b5a2b905887b8b0d1340ce77b588
gabjcav <69957148+gabjcav@users.noreply.github.com>
2021-03-24 14:33:25 +0100

    adding project info

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,6 +1,17 @@
 import PageTitle from '../components/PageTitle'
 const About = () => {
-  return <PageTitle>About</PageTitle>
+  return (
+    <>
+      <PageTitle>- About -</PageTitle>
+      <div className="container about">
+        <div>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default About
